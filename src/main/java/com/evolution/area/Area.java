40e21318d5f3d1ac8e13fa9341ddd86d7df3ff47
Vladimir Voitov <vvoitov@pos-soft.ru>
2020-03-08 18:34:45 +0300

    initial

---
+++ src/main/java/com/evolution/area/Area.java
@@ -0,0 +1,27 @@
+package com.evolution.area;
+
+public class Area {
+	private int width;
+	private int heigh;
+
+	public Area (int width, int heigh){
+		this.width = width;
+		this.heigh = heigh;
+	}
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeigh() {
+		return heigh;
+	}
+
+	public void setHeigh(int heigh) {
+		this.heigh = heigh;
+	}
+
+}
